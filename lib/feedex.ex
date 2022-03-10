a388817b8350efd34c6e1206191d38b091982422
29decibel <mikeli@fastmail.fm>
2022-03-09 18:28:48 -0800

    parse all example feeds

--- lib/feedex.ex
+++ lib/feedex.ex
@@ -1,18 +1,87 @@
+defmodule Feedex.Native do
+  use Rustler,
+    otp_app: :feedex,
+    crate: :feedex_native
+
+  def parse_rss(_arg1), do: :erlang.nif_error(:nif_not_loaded)
+  def parse_atom(_arg1), do: :erlang.nif_error(:nif_not_loaded)
+end
+
+defmodule TeslaClient do
+  use Tesla
+
+  adapter(Tesla.Adapter.Hackney)
+
+  # defaults to 5
+  plug(Tesla.Middleware.FollowRedirects, max_redirects: 3)
+end
+
 defmodule Feedex do
-  @moduledoc """
-  Documentation for `Feedex`.
-  """
+  # https://gist.github.com/sasa1977/5967224
+  import Record, only: [defrecord: 2, extract: 2]
+  defrecord :xmlElement, extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl")
+  defrecord :xmlText, extract(:xmlText, from_lib: "xmerl/include/xmerl.hrl")
+  defrecord :xmlAttribute, extract(:xmlAttribute, from_lib: "xmerl/include/xmerl.hrl")
 
-  @doc """
-  Hello world.
+  def parse_example_opml do
+    xml = File.read!("./examples/Subscriptions-MineReads.opml")
+    {doc, _} = xml |> :binary.bin_to_list() |> :xmerl_scan.string()
 
-  ## Examples
+    :xmerl_xpath.string('//outline', doc)
+    |> Enum.map(&attr(&1, "xmlUrl"))
+    |> Enum.filter(&(!is_nil(&1)))
+  end
 
-      iex> Feedex.hello()
-      :world
+  def parse_feed_url(url) do
+    case TeslaClient.get(url) do
+      {:ok, %{body: body, headers: headers}} ->
+        content_type =
+          headers
+          |> Enum.find_value(nil, fn {_k, v} ->
+            String.contains?(v, "application/json")
+          end)
 
-  """
-  def hello do
-    :world
+        if content_type do
+          result = Jason.decode!(body)
+          IO.inspect(result |> Map.keys())
+        else
+          case Feedex.Native.parse_rss(body) do
+            %{"Err" => _err} ->
+              case Feedex.Native.parse_atom(body) do
+                %{"Err" => err} ->
+                  IO.inspect("Can not parse #{url} - #{err}")
+
+                {:error, err} ->
+                  IO.inspect("Can not parse #{url} - #{err}")
+
+                r ->
+                  IO.inspect(r |> Map.keys())
+                  nil
+              end
+
+            %{"Ok" => result} ->
+              IO.inspect(result |> Map.keys())
+
+              {:ok, result}
+          end
+        end
+
+      _ ->
+        "Can not get #{url}"
+    end
+  end
+
+  def parse_all_example_urls do
+    parse_example_opml() |> Enum.each(&parse_feed_url(&1))
+  end
+
+  def attr(node, name), do: node |> xpath('./@#{name}') |> extract_attr
+  defp extract_attr([xmlAttribute(value: value)]), do: List.to_string(value)
+  defp extract_attr(_), do: nil
+
+  defp xpath(nil, _), do: []
+
+  defp xpath(node, path) do
+    :xmerl_xpath.string(to_char_list(path), node)
   end
 end
--- native/feedex_native/src/lib.rs
+++ native/feedex_native/src/lib.rs
@@ -1,6 +1,48 @@
-#[rustler::nif]
-fn add(a: i64, b: i64) -> i64 {
-    a + b
+use rustler::{Encoder, Env, NifResult, Term};
+use serde_json::json;
+
+mod atoms {
+    rustler::atoms! {
+        ok,
+        error,
+    }
 }
 
-rustler::init!("Elixir.Feedex.Native", [add]);
+#[rustler::nif]
+fn parse_rss(env: Env, rss_str: String) -> NifResult<Term> {
+    let channel = rss::Channel::read_from(rss_str.as_bytes())
+        .map_err(|err| format!("Unable to parse RSS - ({:?})", err));
+
+    let chanel_json = json!(channel);
+
+    let ser = serde_rustler::Serializer::from(env);
+    let encoded = serde_transcode::transcode(chanel_json, ser)
+        .map_err(|_err| "Unable to encode to erlang terms");
+
+    match encoded {
+        Ok(term) => Ok(term),
+        Err(error_message) => Ok((atoms::error(), error_message).encode(env)),
+    }
+}
+
+use atom_syndication::Feed;
+
+#[rustler::nif]
+fn parse_atom(env: Env, rss_str: String) -> NifResult<Term> {
+    match Feed::read_from(rss_str.as_bytes()) {
+        Ok(feed) => {
+            let chanel_json = json!(feed);
+
+            let ser = serde_rustler::Serializer::from(env);
+            let encoded = serde_transcode::transcode(chanel_json, ser)
+                .map_err(|_err| "Unable to encode to erlang terms");
+            match encoded {
+                Ok(term) => Ok(term),
+                Err(error_message) => Ok((atoms::error(), error_message).encode(env)),
+            }
+        }
+        Err(err) => Ok((atoms::error(), err.to_string()).encode(env)),
+    }
+}
+
+rustler::init!("Elixir.Feedex.Native", [parse_atom, parse_rss]);
